dcf15f6a9afa620f5752889c4765d42aa859b19d
quanticc <quanticc@users.noreply.github.com>
2018-08-26 20:49:25 -0300

    Fix gateway test

--- gateway/src/test/java/discord4j/gateway/GatewayTest.java
+++ gateway/src/test/java/discord4j/gateway/GatewayTest.java
@@ -17,6 +17,7 @@
 
 package discord4j.gateway;
 
+import discord4j.common.websocket.CloseStatus;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.WebSocketFrame;
 import org.junit.Before;
